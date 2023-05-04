c3ae862bbce10fe29cef2565a701e8250ba96215
NiceTulipe <lyubovchenko@yandex.ru>
2023-05-05 00:54:42 +0300

    fixed tz13 try3

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.item.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import ru.practicum.shareit.user.model.User;
 
 import javax.validation.constraints.Positive;
