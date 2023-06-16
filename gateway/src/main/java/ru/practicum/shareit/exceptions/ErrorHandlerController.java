b2db04cc10eb83f2f99d1d71f260a92d0c2bd1b6
NiceTulipe <lyubovchenko@yandex.ru>
2023-06-16 15:24:25 +0300

    tz16 codestyle 1 coverage 1

---
+++ gateway/src/main/java/ru/practicum/shareit/exceptions/ErrorHandlerController.java
@@ -0,0 +1,70 @@
+package ru.practicum.shareit.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestValueException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ValidationException;
+import java.util.Map;
+import java.util.Objects;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandlerController {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFoundException(final ObjectNotFoundException exception) {
+        log.warn("Error! Not Found, server status: '{}' text message: '{}'", HttpStatus.NOT_FOUND, exception.getMessage());
+        return Map.of("Not found object", exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleNullPointerException(final NullPointerException exception) {
+        log.warn("Error! NullPointer, server status: '{}' text message: '{}'",
+                HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        return Map.of("Null detected, check your actions", exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handlerValidationException(final ValidationException exception) {
+        log.warn("Error! Validation fault, server status: '{}' text message: '{}'",
+                HttpStatus.BAD_REQUEST, exception.getMessage());
+        return Map.of("Validation fault, check your actions", exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handlerValidException(final MethodArgumentNotValidException exception) {
+        log.warn("Error! Validation fault, server status: '{}' text message: '{}'",
+                HttpStatus.BAD_REQUEST, exception.getMessage());
+        return Map.of("Validation object fault, check your actions", exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleThrowable(final Throwable exception) {
+        log.warn("Error! Server status: '{}' text message: '{}'", HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        return Map.of("Server Error", exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleRequestFailedException(MissingRequestValueException exception) {
+        log.warn("Error! RequestValueMissing: '{}' text message: '{}'", HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        return Map.of("RequestValueMissing", Objects.requireNonNull(exception.getMessage()));
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequestException(final RequestFailedException exception) {
+        log.warn("400 {}", exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
+}
---
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -0,0 +1,20 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * TODO Sprint add-controllers.
+ */
+@Data
+@AllArgsConstructor
+@Builder
+public class ItemDto {
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final Boolean available;
+    private final Long requestId;
+}
+
